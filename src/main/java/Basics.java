d38819ee17315605bd708d8ab272fd2fbc1630fb
MalHus <malaikah.hussain@mail.utoronto.ca>
2022-10-04 16:49:38 -0400

    Complete!

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,8 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
-        /* test commit */
+        System.out.println("Hello World");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -63,7 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -100,8 +99,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
-
+        for (int z = 10; z >= 0; z--) {
+            System.out.println("Current count: " + z);
+        }
     }
 
     /**
@@ -133,6 +133,7 @@ public class Basics {
      *                   spaces.
      * @return           The first letter of every word in to_split
      */
+
     public static String split(String to_split) {
         /* TODO (Task 4): Complete this method body.
          *                The String methods .split and .charAt may be helpful,
@@ -141,9 +142,16 @@ public class Basics {
          *                separated by spaces, so no loops are required, though
          *                you may use them if you wish.
          */
-        StringBuilder ret = new StringBuilder();
 
-        // Fill in the rest of the body here
+        String[] words = to_split.split(" ");
+        StringBuilder ret = new StringBuilder();
+        ret.append(words[0].charAt(0));
+        ret.append(words[1].charAt(0));
+        ret.append(words[2].charAt(0));
+        ret.append(words[3].charAt(0));
+        ret.append(words[4].charAt(0));
+        ret.append(words[5].charAt(0));
+        ret.append(words[6].charAt(0));
 
         return ret.toString();
     }
@@ -172,6 +180,17 @@ public class Basics {
          *                (e.g. arr[i] gives you the item at index i).
          */
 
+        int the_length = arr.length;
+
+        if (the_length == 0) {
+            return current_sum;
+        } else if (the_length == 1) {
+            return current_sum;
+        } else {
+            for (int i = 1; i <= (the_length - 1); i+=2) {
+                current_sum += arr[i];
+            }
+        }
         return current_sum;
     }
 
